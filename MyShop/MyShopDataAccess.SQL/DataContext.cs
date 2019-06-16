d7826f8a00ba826c727533352d86551de341d403
LGOscar <51393877+lgoscar@users.noreply.github.com>
2019-06-16 18:01:08 -0400

    Added Order Creation

--- MyShop/MyShopDataAccess.SQL/DataContext.cs
+++ MyShop/MyShopDataAccess.SQL/DataContext.cs
@@ -21,5 +21,7 @@ namespace MyShopDataAccess.SQL
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
     }
 }
